e8e41e5c486d820abb854e3315552062cf6c7d89
Murad <mchowdh200@gmail.com>
2021-12-01 13:48:42 -0700

    add script to regenotype samples with smoove

---
+++ regenotype.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+bam=$1
+fasta=$2
+name=$3
+sites_vcf=$4
+outdir=$4
+
+# regenotype the sample bam using the merged sites vcf
+# additionally, annotate sites with duphold which provides
+# various coverage based metrics which can be used for
+# filtering false positive duplications and deletions
+smoove genotype --duphold \
+       --removepr \
+       --processes 4 \
+       --fasta $fasta \
+       --vcf $sites_vcf \
+       --name $name-joint \
+       --outdir $outdir \
+       $bam
