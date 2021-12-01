cff477e92a68fc3ac95f5c3e92a6c28d28a6fea6
Murad <mchowdh200@gmail.com>
2021-12-01 10:11:51 -0700

    increase mosdepth window size

--- mosdepth.sh
+++ mosdepth.sh
@@ -4,7 +4,10 @@ bam=$1
 fasta=$2
 prefix=$3
 
-mosdepth --by 100 --fast-mode --no-per-base --chrom ssa01 \
+# calculate coverage of a bam with mosdepth.
+# to speed things up we are using samples with only
+# chromosome ssa01.
+mosdepth --by 10000 --fast-mode --no-per-base --chrom ssa01 \
          --fasta $fasta \
          $prefix \
          $bam
