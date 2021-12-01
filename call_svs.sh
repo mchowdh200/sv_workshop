b38e97e214f54e7fc52fbaeb108df4aced2cc86d
Murad <mchowdh200@gmail.com>
2021-12-01 12:00:52 -0700

    need prpos tags

--- call_svs.sh
+++ call_svs.sh
@@ -7,12 +7,13 @@ exclude=$4
 outdir=$5
 
 # call svs using smoove (lumpy+SVTyper)
+# genotyping in this stage also filters out 0/0 genotypes
 smoove call \
        --name $name \
        --fasta $fasta \
        --exclude $exclude \
+       --outdir $outdir \
        --processes 4 \
        --genotype \
-       --removepr \
        $bam
 
