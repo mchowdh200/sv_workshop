dada32219f6a6e1c6370c5cea654514af53b799c
Murad <mchowdh200@gmail.com>
2021-11-30 17:04:58 -0700

    add fasta flag

--- call_svs.sh
+++ call_svs.sh
@@ -1,13 +1,15 @@
 #!/bin/env bash
 
 bam=$1
-name=$2
-exclude=$3
-outdir=$4
+fasta=$2
+name=$3
+exclude=$4
+outdir=$5
 
 # call svs using smoove (lumpy+SVTyper)
 smoove call \
        --name $name \
+       --fasta $fasta \
        --exclude $exclude \
        --processes 4 \
        --genotype \
