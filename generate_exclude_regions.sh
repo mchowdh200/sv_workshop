d18811e6772a494aaf6efce83099a22339e73980
Murad <mchowdh200@gmail.com>
2021-11-30 18:35:42 -0700

    increase distance for merging regions

--- generate_exclude_regions.sh
+++ generate_exclude_regions.sh
@@ -6,7 +6,7 @@ output=$3
 
 # concat the gaps bed and the high cov regions bed,
 # sort by region, then merge regions if they are within
-# 10 base pairs of each other
+# 100 base pairs of each other
 cat $gaps <(cut -f1-3 $high_cov) |
     bedtools sort -i stdin |
-    bedtools merge -d 10 -i stdin > $output
+    bedtools merge -d 100 -i stdin > $output
