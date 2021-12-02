3535a238e1495eaa2aa8684f8180cbd14a3d0517
Murad <mchowdh200@gmail.com>
2021-12-01 22:27:44 -0700

    add comments to sv calling script

--- call_svs.sh
+++ call_svs.sh
@@ -6,8 +6,22 @@ name=$3
 exclude=$4
 outdir=$5
 
-# call svs using smoove (lumpy+SVTyper)
+### call svs using smoove (lumpy+SVTyper)
 # genotyping in this stage also filters out 0/0 genotypes
+### paramters
+# name: This will be a prefix for results vcf
+# fasta: Reference genome
+# exclude: The set of regions we compiled earlier.
+#          these regions will not be taken into consideration.
+# outdir: Output directory
+# processes: Number of threads to use.  > 4 will basically have no effect.
+#            Its better to parallelize over samples in most cases.   
+# genotype: Genotype regions after calling.  We'll be regenotyping later, but
+#           this step is useful in order to filter out regions deemed to be 0/0
+# noextrafilters: This flag is here strictly for speeding up the demonstration.
+#                 Usually better to leave this flag out so that further filtering
+#                 is performed to refine the results more.
+# bam: The input alignment file
 smoove call \
        --name $name \
        --fasta $fasta \
